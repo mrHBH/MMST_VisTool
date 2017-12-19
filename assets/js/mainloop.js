99922e87598f415b7440204b4ee7c28140ff78fb
mrHBH <hamza.ben_hassen@mailbox.tu-dresden.de>
2017-12-19 16:19:02 +0100

    -added pan and zoom compatibility

--- assets/js/mainloop.js
+++ assets/js/mainloop.js
@@ -1,4 +1,4 @@
-	var triples = [
+var triples = [
   			{subject:"ex:ThaiLand", 	predicate:"ex:hasFood", 	object:"ex:TomYumKung"},
 			{subject:"ex:TomYumKung", 	predicate:"ex:isFoodOf", 	object:"ex:ThaiLand"},
   			{subject:"ex:TomYumKung", 	predicate:"rdf:type", 		object:"ex:SpicyFood"},
@@ -10,13 +10,17 @@
   			{subject:"ex:lemon", 		predicate:"ex:hasTaste", 	object:"ex:sour"},
   			{subject:"ex:chilly", 		predicate:"ex:hasTaste", 	object:"ex:spicy"}
   		];
-		
-	var svg = d3.select("#svg-body").append("svg")
-				.attr("width", 800)
-				.attr("height", 600)
-				;
-				
-	var force = d3.layout.force().size([800, 600]);
+
+var svg = d3.select("#svg-body")
+  .append("svg")
+  .attr("width", "100%")
+  .attr("height",'100%' )
+  .call(d3.behavior.zoom().on("zoom", function () {
+    svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+  }))
+  .append("g")
+
+var force = d3.layout.force().size([800, 600]);
 	
 	var graph = triplesToGraph(triples);
 	
